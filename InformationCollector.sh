faf5db6128cada303cbe72ea2463542002eac438
Espen Fjellvær Olsen <espen@mrfjo.org>
2013-04-29 22:46:34 +0200

    Add license header

--- InformationCollector.sh
+++ InformationCollector.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
-# Copyright (c) 2013 Espen Fjellvær Olsen
-#
+
 # This script will collect information on a running system and hopefully
 # gather something useful for later analysis.
+
+# Copyright (c) 2013 Espen Fjellvær Olsen
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 echo "Information collector system"
 tmp=`mktemp -d`
 logDate=2 # Days of logs to collect
