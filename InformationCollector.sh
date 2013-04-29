8c203dcc253f8ee3fad9817b9b35b680aa849026
Espen Fjellvær Olsen <espen@mrfjo.org>
2013-04-29 23:15:19 +0200

    Beautify, and add some more stuff

--- InformationCollector.sh
+++ InformationCollector.sh
@@ -19,31 +19,128 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 echo "Information collector system"
+exec 6>&1
+exec 2>/dev/null
+
 tmp=`mktemp -d`
 logDate=2 # Days of logs to collect
+version=1
 
 # Try to collect relevant information about the OS
 function collectSystemInformation {
-    uname -a > $tmp/uname
-    cat /etc/*release > $tmp/release
-    cat /etc/*version >> $tmp/release
-    type lsb_release &> /dev/null && lsb_release -a > $tmp/lsb
-    lsof -n > $tmp/lsof
+    exec > $tmp/systemInformation
+
+    echo "*************************************"
+    echo "*** Standard system information ***"
+    echo "*************************************"
+    uname -a
+    type lsb_release &> /dev/null && lsb_release -a
+    cat /etc/*release
+    cat /etc/*version
+    uptime
+
+    echo "*************************************"
+    echo "*** Kernel messages ***"
+    echo "*************************************"
+    dmesg
+
+    echo "*************************************"
+    echo "*** Hardware information ***"
+    echo "*************************************"
+    cat /proc/cpuinfo
+    echo
+    free -m
+    
+    echo "*********************************"
+    echo "*** Virtual memory statistics ***"
+    echo "*********************************"
+    vmstat
+
+    echo "***********************************"
+    echo "*** Top 5 CPU eating process ***"
+    echo "***********************************"
+    ps auxf | sort -nr -k 3 | head -5 
+
+    echo "***********************************"
+    echo "*** Top 5 memory eating process ***"
+    echo "***********************************"
+    ps auxf | sort -nr -k 4 | head -5 
+
+    echo "*************************************"
+    echo "*** Open files ***"
+    echo "*************************************"
+    lsof -n
+    
+}
+
+function collectUsers {
+    exec > $tmp/userInformation
+    
+    echo "**************************************"
+    echo "*** Users logged in ***"
+    echo "**************************************"
+    who -H -a
+    
+    echo "**************************************"
+    echo "*** Last logged in users ***"
+    echo "**************************************"
+    last
 }
 
 # Collect information about the network
 function collectNetworkInformation {
-    netstat -lapn > $tmp/netstat
-    route -n > $tmp/route
-    /sbin/ifconfig -a > $tmp/ifconfig
+    exec > $tmp/networkInformation
+
+    echo "**************************************"
+    echo "*** Network routing ***"
+    echo "**************************************"
+    netstat -nr
+
+    echo "**************************************"
+    echo "*** Interface information  ***"
+    echo "**************************************"
+    
+    /sbin/ifconfig -a
+
+    echo "**************************************"
+    echo "*** Interface traffic information ***"
+    echo "**************************************"
+    netstat -i    
+
+    echo "**************************************"
+    echo "*** Resolv.conf ***"
+    echo "**************************************"
+    cat /etc/resolv.conf
+
+    echo "**************************************"
+    echo "*** Open network connections  ***"
+    echo "**************************************"   
+    netstat -lapn  
 }
 
 # Collect information about running processes
 function collectProcessInformation {
-    ps auxwwwf > $tmp/ps
-    ls -l /proc/*/cwd > $tmp/proc
-    ls -l /proc/*/exe >> $tmp/proc
-    grep -a ^ /proc/*/cmdline >> $tmp/proc-cmdline   
+    exec > $tmp/processes
+    echo "**************************************"
+    echo "*** Running processes ***"
+    echo "**************************************"
+    ps auxwwwf
+
+    echo "**************************************"
+    echo "*** Directory of running processes ***"
+    echo "**************************************"
+    ls -l /proc/*/cwd
+
+    echo "**************************************"
+    echo "*** Executable of running processes ***"
+    echo "**************************************"
+    ls -l /proc/*/exe
+
+    echo "**************************************"
+    echo "*** Arguments of running processes ***"
+    echo "**************************************"
+
+    grep -a ^ /proc/*/cmdline
 }
 
 
@@ -54,11 +151,11 @@ function collectLogs {
 }
 
 
-collectSystemInfomration
+collectSystemInformation
+collectUsers
 collectNetworkInformation
 collectProcessInformation
 collectLogs
-
 
 tar cfz InformationCollector-$(date +%y%m%d).tgz $tmp
 rm -rf $tmp
